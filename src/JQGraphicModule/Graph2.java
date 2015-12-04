b135ec56e42512bf8809f451cc169a7fc0ba25f8
Dylan HJ Chen <hjchen2005@gmail.com>
2015-12-04 14:44:55 -0500

    Create Graph2.java

---
+++ src/JQGraphicModule/Graph2.java
@@ -0,0 +1,24 @@
+// Layouts of the query table
+package JQGraphicModule;
+import javax.swing.*;
+import org.jfree.chart.*;
+import org.jfree.data.*;
+
+public class Graph2 extends JFrame {
+  public Graph2(){
+    initComponents();
+  }
+  @SuppressWarnings("unchecked")
+  
+  private JRadioButton b;
+  private JPanel barras;
+  private JLayeredPane capas;
+  private JTable datos;
+  private JButton jButton1;
+  private JLabel jLable1;
+  private JScrollPane jScrollPane;
+  private JRadioButton l;
+  private JPanel lineas;
+  private JRadioButton p;
+  private JPanel pastel;
+}
