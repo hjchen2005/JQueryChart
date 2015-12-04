bd999325b0ad5c22d4782938f1c8eeadb668a453
Dylan HJ Chen <hjchen2005@gmail.com>
2015-12-04 14:54:11 -0500

    Update Graph2.java

--- src/JQGraphicModule/Graph2.java
+++ src/JQGraphicModule/Graph2.java
@@ -3,12 +3,23 @@ package JQGraphicModule;
 import javax.swing.*;
 import org.jfree.chart.*;
 import org.jfree.data.*;
+import java.awt.event.*;
 
 public class Graph2 extends JFrame {
   public Graph2(){
     initComponents();
+    this.setLocationRelativeTo(null);
+    this.lineas.setVisible(false);
+    this.pastel.setVisible(false);
+    
+    this.setVisible(true);
   }
   @SuppressWarnings("unchecked")
+  // right click to the radio button Lineas -> event -> action -> action performed
+  private void lActionPerformed(ActionEvent evt){
+    lineas.setVisible(true);
+    
+  }
   
   private JRadioButton b;
   private JPanel barras;
